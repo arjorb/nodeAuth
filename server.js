12bfd3543b2e463b337cff47a826652dc1babba5
John U <arjorb@gmail.com>
2023-08-21 10:28:32 +0200

    creating a db and connect to it

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const dotenv = require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
 app.use(express.json());
+
+//connect to the mongo database
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(res => console.log(`Connected to mongo database`))
+  .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
   res.send(`Hello Am Live ✨ now`);
