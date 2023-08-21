f603dd0b8e4aa9607d5a5d62203594f1ce4a12f3
John U <arjorb@gmail.com>
2023-08-21 10:43:05 +0200

    handling the error in the user model

--- models/User.js
+++ models/User.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-  {
-    firstname: { type: 'string', required: true, min: 3 },
-    lastname: { type: 'string', required: true, min: 3 },
-    email: { type: 'string', required: true },
-    password: { type: 'string', required: true, min: 8 },
-  },
-  { timestamps: { type: 'boolean', required: true } }
-);
+const userSchema = new Schema({
+  firstname: { type: String, required: true, min: 3, max: 255 },
+  lastname: { type: String, required: true, min: 4, max: 255 },
+  email: { type: String, required: true, min: 6, max: 255 },
+  password: { type: String, required: true, min: 8, max: 1024 },
+  date: { type: Date, default: new Date.now() },
+});
 
-module.exports = mongoose.Model('User', userSchema);
+module.exports = mongoose.model('User', userSchema);
