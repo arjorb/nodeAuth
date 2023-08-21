f418ad0466f85739693016956f1d27ee52fec0f0
John U <arjorb@gmail.com>
2023-08-21 15:08:05 +0200

    creating authVerify to protect private route of the admin

---
+++ routes/authVerify.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+const verify = (req, res, next) => {
+  const token = req.header('auth-token');
+  if (!token) {
+    res.status(403).send(`Not authorized`);
+  }
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    next();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+module.exports = verify;
